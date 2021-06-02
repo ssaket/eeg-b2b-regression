ba6d03b72775ae5d7c3198c34c6af4d75502a0bf
ssaket <skt.saurabh@gmail.com>
2021-06-02 11:10:32 +0200

    analyse dataset

--- utils.jl
+++ utils.jl
@@ -3,13 +3,13 @@ using MAT, DelimitedFiles
 
 # get data from EEGLab using PyMNE 
 function read_eeglab(dataPath::String)
-    raw = PyMNE.io.read_raw_eeglab(dataPath, preload = true)
+    raw = PyMNE.io.read_raw_eeglab(dataPath, preload=true)
     raw.resample(128) # if you want speed ;)
     # get events dataframe
     events = DataFrame(
-        latency = raw.annotations.onset,
-        types = string.(raw.annotations.description),
-        duration = raw.annotations.duration,
+        latency=raw.annotations.onset,
+        types=string.(raw.annotations.description),
+        duration=raw.annotations.duration,
     )
     data = raw.get_data()
     return data, events
@@ -20,7 +20,7 @@ function read_raw_eeglab(filename)
     file = MAT.matopen(filename)
     EEG = read(file, "EEG")  # open file
     function parse_struct(s::Dict)
-        return DataFrame(map(x -> dropdims(x, dims = 1), values(s)), collect(keys(s)))
+        return DataFrame(map(x -> dropdims(x, dims=1), values(s)), collect(keys(s)))
     end
     evts_df = parse_struct(EEG["event"])
     chanlocs_df = parse_struct(EEG["chanlocs"])
@@ -39,7 +39,7 @@ function read_raw_eeglab(filename)
         data = EEG["data"]
     end
     if (ndims(data) == 3) & (size(data, 3) == 1)
-        data = dropdims(data, dims = 3)
+        data = dropdims(data, dims=3)
     end
     return data, srate, evts_df, chanlocs_df, EEG
 end
