fca6e2e5454cc9e54ec61984d2f8457c7ff1d425
Saket Saurabh <ssaurabh@k15t.com>
2021-07-02 03:40:27 +0200

    add module

--- regression.jl
+++ regression.jl
@@ -1,69 +1,109 @@
+module B2BRegression
+# imports
 using Unfold, StatsModels
-
-# init Plots
-# using Plots # I dont use Plots but Makie.jl
-# plotly()
 include("utils.jl")
 
+# get massunivariate gamma
+function get_massunivariate_gamma(path, formula, event_types = undef)
+    data, events = read_eeglab_with_all_events(path, sfreq = 128)
 
-# define dataset path
-path = "data/sub-45/eeg/sub-45_task-WLFO_eeg.set"
-if !isfile(path)
-    path = "/store/data/WLFO/derivatives/preproc_agert/sub-45/eeg/sub-45_task-WLFO_eeg.set"
+    # select only fixation
+    events = events[events.type.=="fixation", :]
+    events[!, :sac_amplitude] = Float64.(events.sac_amplitude)
+    events[!, :sac_vmax] = Float64.(events.sac_vmax)
+
+    beta, times = Unfold.epoch(data = data, tbl = events, τ = (-0.3, 0.5), sfreq = 128) # cut the data into epochs
+    beta[:, :, isnan.(events.sac_vmax)] .= missing # to run with sac_vmax
+    se_solver = solver = (x, y) -> Unfold.solver_b2b(x, y, cross_val_reps = 5)
+
+    # Generate Designmatrix & fit mass-univariate model (one model per epoched-timepoint) 
+    model, results_expanded =
+        Unfold.fit(UnfoldLinearModel, f, events, beta, times, solver = se_solver)
+    return results_expanded
 end
-# raw, events = read_mne_eeglab(path, 128)
-# data = raw.get_data()
 
-# Data is typically interpreted as channel x time (with basisfunctions) or channel x time x epoch (for mass univariate)
-data, events = read_eeglab_with_all_events(path, sfreq=128)
- 
-# define formula 
-f = @formula 0~1 +  sac_amplitude # also tried + sac_vmax with interesting results
+# plot massunivariate gamma
+function plot_massunivariate_gamma(path, formula, event_types = undef)
 
+    data, events = read_eeglab_with_all_events(path, sfreq = 128)
 
-# select only fixation
-events = events[events.type .== "fixation",:]
-events[!,:sac_amplitude] = Float64.(events.sac_amplitude)
-events[!,:sac_vmax] = Float64.(events.sac_vmax)
+    # select only fixation
+    events = events[events.type.=="fixation", :]
+    events[!, :sac_amplitude] = Float64.(events.sac_amplitude)
+    events[!, :sac_vmax] = Float64.(events.sac_vmax)
 
+    beta, times = Unfold.epoch(data = data, tbl = events, τ = (-0.3, 0.5), sfreq = 128) # cut the data into epochs
+    beta[:, :, isnan.(events.sac_vmax)] .= missing # to run with sac_vmax
+    se_solver = solver = (x, y) -> Unfold.solver_b2b(x, y, cross_val_reps = 5)
 
+    # Generate Designmatrix & fit mass-univariate model (one model per epoched-timepoint) 
+    model, results_expanded =
+        Unfold.fit(UnfoldLinearModel, f, events, beta, times, solver = se_solver)
+    plot_results(results_expanded, layout_x = :basisname)
+end
 
-#= Source: Unfold Documentation
-Since, Deconvolution works on continuous data, to compare it to the “normal” use-case, we have to epoch it. 
-Data cleaning is doing in the fuction definition 
-We additionally remove trials from unfold.X that were removed during epoching 
-=#
-beta, times = Unfold.epoch(data=data, tbl=events, τ=(-0.3, 0.5), sfreq=128) # cut the data into epochs
+# get time-expanded gamma
+function get_time_expanded_gamma(path, formula, event_types = undef)
+    data, events = read_eeglab_with_all_events(path, sfreq = 128)
 
-beta[:,:,isnan.(events.sac_vmax)] .= missing # to run with sac_vmax
+    # select only fixation
+    events = events[events.type.=="fixation", :]
+    events[!, :sac_amplitude] = Float64.(events.sac_amplitude)
+    events[!, :sac_vmax] = Float64.(events.sac_vmax)
 
-# Special solver solver_lsmr_b2b with b2b regression
+    beta, times = Unfold.epoch(data = data, tbl = events, τ = (-0.3, 0.5), sfreq = 128) # cut the data into epochs
+    beta[:, :, isnan.(events.sac_vmax)] .= missing # to run with sac_vmax
+    se_solver = solver = (x, y) -> Unfold.solver_b2b(x, y, cross_val_reps = 5)
 
-# depending on unfold version, you might not be able to use the named argument, but have to put solver_b2b(x,y,1)
-se_solver = solver = (x, y) -> Unfold.solver_b2b(x, y,cross_val_reps = 5)
+    b1 = firbasis(τ = (-1, 1), sfreq = 20, name = "basisA")
+    f1 = @formula 0 ~1 + sac_amplitude
 
+    # Generate Designmatrix & fit time-expanded model(modeling linear overlap).
+    # old method
+    # model,results_expanded = Unfold.fit(UnfoldLinearModel,f,evts,data,b1) 
+    model_new, result_long_new = fit(
+        UnfoldLinearModel,
+        Dict(0 => (f1, b1)),
+        evts,
+        data,
+        eventcolumn = "fixation",
+    )
+    return result_long_new
+end
 
-# Generate Designmatrix & fit mass-univariate model (one model per epoched-timepoint) 
-model, results_expanded = Unfold.fit(UnfoldLinearModel, f, events, beta, times, solver=se_solver)
+# plot time expanded gamma
+function plot_time_expanded_gamma(path, f1, b1, event_types = undef)
+    data, events = read_eeglab_with_all_events(path, sfreq = 128)
 
-# include("dev/Unfold/src/plot.jl") # install Makie first, also run ]dev --local Unfold   to get a "local copy" of Unfold in the folder ./dev/Unfold
+    # select only fixation
+    events = events[events.type.=="fixation", :]
+    events[!, :sac_amplitude] = Float64.(events.sac_amplitude)
+    events[!, :sac_vmax] = Float64.(events.sac_vmax)
 
-plot_results(results_expanded,layout_x=:basisname) # the layout_x is needed because group=nothing and the plotting function doesnt like that...
+    beta, times = Unfold.epoch(data = data, tbl = events, τ = (-0.3, 0.5), sfreq = 128) # cut the data into epochs
+    beta[:, :, isnan.(events.sac_vmax)] .= missing # to run with sac_vmax
+    se_solver = solver = (x, y) -> Unfold.solver_b2b(x, y, cross_val_reps = 5)
 
-#--- didnt look further than that, but noticed I havent implemented B2B for 2D data yet (necessary for deconvolution) - but it should be very simple to do.
+    # Generate Designmatrix & fit time-expanded model(modeling linear overlap).
+    # old method
+    model, results_expanded = Unfold.fit(UnfoldLinearModel, f1, events, data, b1)
+    # model_new, result_long_new =
+    #     fit(UnfoldLinearModel, Dict(0 => (f1, b1)), events, data, eventcolumn = :type, solver = se_solver)
+    plot_results(results_expanded, layout_x = :basisname)
+end
 
-# select a channel? Is it necessary?plo
-# results = results_expanded[results_expanded.channel.==1,:]
-# plot(x = results[:colname_basis], y = results[:estimate])
+# main funtion
+function run()
+    path = "data/sub-45/eeg/sub-45_task-WLFO_eeg.set"
+    if !isfile(path)
+        path = "/store/data/WLFO/derivatives/preproc_agert/sub-45/eeg/sub-45_task-WLFO_eeg.set"
+    end
 
-# # Timexpanded Univariate Linear
-# b1 = firbasis(τ=(-1,1),sfreq=20,name="basisA")
-# f1  = @formula 0~1+sac_amplitude
+    # f = @formula 0 ~1 + sac_amplitude # also tried + sac_vmax with interesting results
+    b1 = firbasis(τ = (-1, 1), sfreq = 120, name = "basisA")
+    f1 = @formula 0~1 + sac_amplitude + sac_vmax
+    plot_time_expanded_gamma(path, f1, b1)
+end
+end
 
-# # Generate Designmatrix & fit time-expanded model(modeling linear overlap).
-# model,results_expanded = Unfold.fit(UnfoldLinearModel,f,evts,data,b1)
-# model_new, result_long_new = fit(UnfoldLinearModel,Dict(0=>(f1,b1)),evts,data,eventcolumn="fixation")
-
-
-# TODO
-# Add plotting function 
+B2BRegression.run()
