7ea99043a13717a0b95711a1f15887f52740f11e
Saket Saurabh <ssaurabh@k15t.com>
2021-07-05 04:20:53 +0200

    cleanup

--- simulation.jl
+++ simulation.jl
@@ -3,39 +3,67 @@ using DataFrames, Distributions
 using Unfold, StatsModels
 
 include("utils.jl")
-ntime = 100
-ntrials = 50
-nchannels = 30
 
-function simulate_epoch_data(ntime, ntrials, nchannels; event_ids=undef)
-    
-    event_names= ["id", "cat_A" ,"cond_A", "cond_B"]
+hanning_windows(time) = DSP.hanning(time)
+hamming_windows(time) = DSP.hamming(time)
+hanwin_windows(time) = DSP.hanning_winplot(time)
 
-    mv_dist = MvNormal(ones(2), [1 0.5;0.5 1])
+random_noise(nchannels, ntime, ntrials) = (noiseLevels = Array(1:nchannels) ./ nchannels;
+noiseLevels .* randn(nchannels, ntime, ntrials))
+
+frm = @formula 0~1 + cond_A + cat_A
+
+event_ids = Dict(1 => "id", 2 => "cat_A", 3 => "cond_A", 4 => "cond_B")
+
+struct EpochData
+    events::DataFrames
+    data::Array
+    times::Array
+
+    EpochData(events, data, times) = new(events, data, times)
+end
+
+
+function simulate_epoch_data(
+    ntime = 100,
+    ntrials = 50,
+    nchannels = 30;
+    event_ids = event_ids;
+    noise_generator = random_noise;
+    windows = hanning_windows
+)
+    mv_dist = MvNormal(ones(2), [1 0.5; 0.5 1])
 
     X = rand(mv_dist, ntrials)
     X = hcat(ones(ntrials), rand(0:1, ntrials), X')
+    col_names = [Symbol(v) for (k, v) in event_ids]
+    events = DataFrame(X, col_names)
 
-    b = DSP.hanning(ntime)
-    coef = [0,3,1,2.]
-    Yhat = X * (repeat(b', length(event_names)) .* coef)
-    
-    noiseLevels = Array(1:nchannels)./nchannels
-    noise = noiseLevels .* randn(nchannels,ntime,ntrials)
+
+    b = windows(ntime)
+    coef = [0, 3, 1, 2.0]
+
+    @assert length(event_ids) == length(coef)
+
+    Yhat = X * (repeat(b', length(event_ids)) .* coef)
+
+    noise = noise_generator(nchannels, ntime, ntrials)
 
     Yhat = reshape(Yhat', 1, ntime, ntrials)
-    Y = repeat(Yhat,nchannels) + noise
-
-    col_names = [Symbol(c) for c in event_names]
-    events = DataFrame(X,col_names)
-
-    se_solver = (x, y) -> Unfold.solver_b2b(x, y, cross_val_reps = 5)
-
-    # Generate Designmatrix & fit mass-univariate model (one model per epoched-timepoint) 
-    @info "fit mass-univariate model"
+    beta = repeat(Yhat, nchannels) + noise
     times = Array(1:ntime)
-    f = @formula 0~1 + cond_A + cat_A
-    model, results_expanded =
-        Unfold.fit(UnfoldLinearModel, f, events, Y, times, solver = se_solver)
-    plot_results(results_expanded, layout_x = :basisname)
+
+    epoch_data = EpochData(events, beta, times)
+
+    return epoch_data
 end
+
+
+se_solver = (x, y) -> Unfold.solver_b2b(x, y, cross_val_reps = 5)
+
+# Generate Designmatrix & fit mass-univariate model (one model per epoched-timepoint) 
+@info "fit mass-univariate model"
+
+model, results_expanded =
+    Unfold.fit(UnfoldLinearModel, f, events, Y, times, solver = se_solver)
+plot_results(results_expanded, layout_x = :basisname)
