e0c4cc2c326f9c5e5a49a34682a8f9f02108750f
Saket Saurabh <ssaurabh@k15t.com>
2021-07-07 11:57:32 +0200

    update functions

--- analysis.jl
+++ analysis.jl
@@ -15,6 +15,9 @@ md"""
 # Encoding and Decoding Analysis using Back-2-Back regression 
 """
 
+# ╔═╡ 4cda793b-a766-45de-9fe6-06ffb5a4d847
+
+
 # ╔═╡ bf87faea-f9b3-4403-867a-422a57034d31
 md"""
 ## Introduction
@@ -55,6 +58,12 @@ md"""
 ## Observations
 """
 
+# ╔═╡ 0b361dee-8e1e-4ddc-807d-789de6d5e498
+md"""
+we plot the gamma of Intercept and Saccades amplitude.\
+$y = \beta_0 + \beta_1 * sacc\_amp$
+"""
+
 # ╔═╡ 5524c3fe-0c6d-48df-80dc-98ddf5172515
 begin
 	f = @formula 0~1 + sac_amplitude; 
@@ -62,12 +71,26 @@ begin
 	plot_results(res_exp,layout_x=:basisname);
 end
 
+# ╔═╡ 044c89eb-9b08-41ab-a4f6-06dd63f35df2
+md"""
+
+we plot the gamma of Intercept and Saccades Velocity.\
+$y = \beta_0 + \beta_1 * sacc\_vmax$
+
+"""
+
 # ╔═╡ 7ad9d3d7-d77b-483b-8e26-bed435345ffd
 begin
 	f_vmax = @formula 0~1 + sac_vmax
 	_, res_vmax = get_gamma(f_vmax, events, beta, times);
 	plot_results(res_vmax,layout_x=:basisname)
 end
+
+# ╔═╡ 8b79b4e0-8f0f-49ba-b29b-6f5556897847
+md"""
+we plot the gamma of Intercept, Saccades amplitude and Saccades Velocity.\
+$y = \beta_0 + \beta_1 * sacc\_amp + \beta_2 * sacc\_vmax$
+"""
 
 # ╔═╡ 31811918-12b5-4540-b6c7-b18ce88e111d
 begin
@@ -77,18 +100,38 @@ begin
 end
 	
 
+# ╔═╡ 54a02c16-48f4-4bdc-ba06-3d6af0a971d7
+md"""
+we plot the gamma of Intercept, human face, saccades amplitude and saccades velocity.\
+$y = \beta_0 + \beta_1 * cat(human\_face) + \beta_2 * sac_amplitude + \beta_3 * sac\_vmax$
+"""
+
+# ╔═╡ c5dc110c-240c-423e-a660-42644f5c7fcb
+begin
+	f_amp_vmax_hf = @formula 0~1 + sac_amplitude + sac_vmax + humanface
+	_, res_amp_vmax_hf = get_gamma(f_amp_vmax_hf, events, beta, times);
+	plot_results(res_amp_vmax_hf,layout_x=:basisname)
+end
+
+# ╔═╡ ae0d9783-726d-4b07-a616-c4980935b3f9
+md"""
+
+# Playground
+
+"""
+
+# ╔═╡ 311758d8-05a2-48fb-9a32-f0e5cbd40d2a
+begin
+	fig = Figure(resolution = (800, 600))
+	pos = fig[1, 1] = plot_results(res_exp,layout_x=:basisname);
+	pos2 = fig[1, 2] = plot_results(res_amp_vmax,layout_x=:basisname)
+end
+
 # ╔═╡ 1ca81b86-15fc-498c-8ec0-deef332a720d
 begin
 	fnew = @formula 0~1 + fix_avgpos_x
 	_, res_amp_vmax_pos = get_gamma(fnew, events, beta, times);
 	plot_results(res_amp_vmax_pos,layout_x=:basisname)
-end
-
-# ╔═╡ 311758d8-05a2-48fb-9a32-f0e5cbd40d2a
-begin
-	fig = Figure()
-	pos = fig[1, 1] = plot_results(res_exp,layout_x=:basisname);
-	pos2 = fig[1, 2] = plot_results(res_amp_vmax,layout_x=:basisname)
 end
 
 # ╔═╡ 59607be3-3048-4298-9fc2-9535ae6a9837
@@ -123,6 +166,7 @@ end
 
 # ╔═╡ Cell order:
 # ╟─369e1538-3855-4d0b-9b17-6f7dd32e9cc9
+# ╠═4cda793b-a766-45de-9fe6-06ffb5a4d847
 # ╟─bf87faea-f9b3-4403-867a-422a57034d31
 # ╠═04ddbcb7-5c2b-4c3b-8cf5-a5b294af8d64
 # ╠═4493bf65-5fec-4be3-bff2-491c2562ce78
@@ -131,10 +175,16 @@ end
 # ╠═f6cf5ad3-5aad-449b-b563-e7f1a1db7c43
 # ╠═cd36c14d-fc59-4327-b807-776f007b1f10
 # ╟─c6c154d4-0a74-4fa9-b0ad-5c52b9377c2d
+# ╟─0b361dee-8e1e-4ddc-807d-789de6d5e498
 # ╠═5524c3fe-0c6d-48df-80dc-98ddf5172515
+# ╟─044c89eb-9b08-41ab-a4f6-06dd63f35df2
 # ╠═7ad9d3d7-d77b-483b-8e26-bed435345ffd
+# ╟─8b79b4e0-8f0f-49ba-b29b-6f5556897847
 # ╠═31811918-12b5-4540-b6c7-b18ce88e111d
-# ╠═1ca81b86-15fc-498c-8ec0-deef332a720d
+# ╟─54a02c16-48f4-4bdc-ba06-3d6af0a971d7
+# ╠═c5dc110c-240c-423e-a660-42644f5c7fcb
+# ╟─ae0d9783-726d-4b07-a616-c4980935b3f9
 # ╠═311758d8-05a2-48fb-9a32-f0e5cbd40d2a
+# ╠═1ca81b86-15fc-498c-8ec0-deef332a720d
 # ╠═59607be3-3048-4298-9fc2-9535ae6a9837
 # ╠═1701be1f-437d-499c-94cd-6b1a1d15a0a3
